afaea4491f9261ccbbb2691414fdbba3fc78a8c9
efeuwoghiren <efeuwoghiren@gmail.com>
2023-02-16 15:47:27 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include<unistd.h>
+#include <unistd.h>
 /**
-* main - A c program that prints a line to the standard error
-* Return: 1 (Success)
-*/
+ *main - Entry
+ *Return: 1
+ */
 int main(void)
 {
- fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);	
-return (1); 
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
