5f8b80a8927a2f3eb8ff2884053a65834b2372c1
efeuwoghiren <efeuwoghiren@gmail.com>
2023-02-17 17:38:13 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ef = 'a';
+
+	while ( ef <= '2')
+	{
+		putchar(ef);
+		ef++;
+	}
+	putchar('\n');
+	return (0);
+}
