f0cac27420fe366947fc37147c5b19cc2c358866
efeuwoghiren <efeuwoghiren@gmail.com>
2023-02-18 06:10:55 -0800

    Print Numbers_3

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,10 +10,12 @@
 
 int main(void)
 {
-	int a;
-	for (a = 0; a < 10; a++)
+	int num;
 
-		printf("%d", a);
-	putchar('\n');
+	for (num = 0; num < 10; num++)
+	printf("%d", num);
+
+	printf("\n");
+
 	return (0);
-}
+}	
