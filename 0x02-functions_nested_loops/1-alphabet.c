7581e96a51d6c9ff59bec0c10a3f7733bc53e848
efeuwoghiren <efeuwoghiren@gmail.com>
2023-02-22 00:17:46 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
  * main -  print_alphabet making the letters
  * Return: void
  */
-void print_alphabet(void) /* function */
+void print_alphabet(void) /* print_alphabet */
 {
 	char c;
 
