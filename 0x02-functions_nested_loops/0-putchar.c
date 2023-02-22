3d9d5a6b52948e69869f8bd870173023a20b1f21
efeuwoghiren <efeuwoghiren@gmail.com>
2023-02-21 17:37:05 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (success)
  */
 
-int manin(void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -8,5 +8,5 @@
  */
 int _putchar(char c)
 {
-	4ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a1return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
