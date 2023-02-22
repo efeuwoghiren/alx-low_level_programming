8e0d33fb174399fc826e3f2c9cb1edc672d81782
efeuwoghiren <efeuwoghiren@gmail.com>
2023-02-21 16:57:35 -0800

    0-putchat

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to output
+ * c: the character to print
+ * Return: 1 (Success)
+ * Return: -1 (on error)
+ */
+int _putchar(char c)
+{
+	4ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a14ad63bf6a1return (write(1, &c, 1));
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+int _putchar(char c);
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+islower(int c);
+isapha(int c);
+print_sign(int n);
+_abs(int);
+print_last_digit(int);
+jack_bauer(void);
+times_table(void);
+add(int, int);
+print_to_98(int n);
+print_times_table(int n);
