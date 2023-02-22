e4a3b1fe79296a959129723402ee64002c3b0d96
efeuwoghiren <efeuwoghiren@gmail.com>
2023-02-21 18:07:37 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet making the letters
+ * Return: viid
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
